86cc848d83e1b3b4439c11a0cbe7ea47e679b2ad
Jessica Kerr <jessitron@gmail.com>
2018-01-07 16:42:42 -0600

    dang it stop committing the build dir

--- build/src/DoesThisExit.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// throwing away a slow promise
-sitAround(10).then(function () { return console.log("yawn"); });
-console.log("end of file reached");
-// it prints "end of file" and then later "yawn" and then exits
-function sitAround(seconds) {
-    return new Promise(function (resolve) { return setTimeout(resolve, seconds * 1000); });
-}
-//# sourceMappingURL=DoesThisExit.js.map
--- build/src/exerciseGitHubCalls.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var stringify = require("json-stringify-safe");
-var githubCalls_1 = require("./githubCalls");
-githubCalls_1.gatherMyRepositoryNames(2).then(function (result) { return Promise.all(result.map(function (r) { return githubCalls_1.gatherCommitCount(r); })); })
-    .then(function (result) {
-    return console.log(result.map(function (s) { return stringify(s, null, 2); }).join("\n"));
-});
-//# sourceMappingURL=exerciseGitHubCalls.js.map
